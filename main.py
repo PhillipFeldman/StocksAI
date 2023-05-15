fd40c6692eaeef001834bc9c5e0d38c73bf45b88
PhillipFeldman <pfeldman@berkeley.edu>
2023-05-15 00:11:20 -0700

    First response

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 import requests
 API_Key = "SvKFFsM0iUpzvx_R8Uc7o5ZaBAxLBEsa"
 
+req = "https://api.polygon.io/v2/aggs/ticker/AAPL/range/1/day/2023-01-09/2023-01-09?apiKey=SvKFFsM0iUpzvx_R8Uc7o5ZaBAxLBEsa"
+
+res = requests.get(req)
+print(res)
 print("Hello World!")
